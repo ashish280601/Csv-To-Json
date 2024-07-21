c432af0237edf74a2fd56841b74c1f1d58a837c2
unknown <asmehra0628@gmail.com>
2024-07-22 00:24:23 +0530

    completed the csv to json builder

---
+++ src/config/db.js
@@ -0,0 +1,28 @@
+import pkg from "pg";
+
+const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
+
+const connectToDB = async() => {
+    try {
+        await pool;
+        console.log("DB connected successfully.");
+    } catch (error) {
+        console.error("Failed to connect to the db", error);
+        // exit the process with the error code
+        process.exit(1);
+    }
+}
+
+export const query = (text, params) => pool.query(text, params);
+export default connectToDB;
