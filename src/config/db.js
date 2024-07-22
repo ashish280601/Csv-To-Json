749c42165ad1a4c9ce7b801d0bb69f8ddb8417a3
unknown <asmehra0628@gmail.com>
2024-07-22 10:04:34 +0530

    added deployment

--- src/config/db.js
+++ src/config/db.js
@@ -2,16 +2,17 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL
+// })
 
 const connectToDB = async() => {
     try {
